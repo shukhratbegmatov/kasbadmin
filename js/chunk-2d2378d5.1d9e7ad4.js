6510387713ef94de88c42c47ac90a1a3516d6ed1
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2023-04-14 00:24:48 +0500

    fix

--- js/chunk-2d2378d5.1d9e7ad4.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2378d5"],{fc35:function(n,u,c){"use strict";c.r(u);var l,e,o=c("2877"),p={},s=Object(o["a"])(p,l,e,!1,null,null,null);u["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d2378d5.1d9e7ad4.js.map
